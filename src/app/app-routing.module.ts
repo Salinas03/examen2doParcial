e3fad0344ddcfd63b7430e6b07ab76a14a8d947e
Diego Salinas <125302353+Salinas03@users.noreply.github.com>
2024-03-11 16:26:10 -0600

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AltaJugadorComponent } from './pages/alta-jugador/alta-jugador.component';
+import { ListaJugadoresComponent } from './pages/lista-jugadores/lista-jugadores.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { AltaClienteComponent } from './pages/alta-cliente/alta-cliente.component';
+import { ListaClientesComponent } from './pages/lista-cliente/lista-cliente.component';
+import { ListadoLibroComponent } from './pages/listado-libro/listado-libro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'altaJugador',
+    component: AltaJugadorComponent
+  },
+  {
+    path: 'listaJugador',
+    component: ListaJugadoresComponent
+  },
+  {
+    path: 'contacto',
+    component: AcercaDeComponent
+  },
+  {
+    path: 'altaCliente',
+    component: AltaClienteComponent
+  },
+  {
+    path: 'listaCliente',
+    component: ListaClientesComponent
+  },
+  {
+    path: 'listadoLibro',
+    component: ListadoLibroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AltaClienteComponent } from './pages/alta-cliente/alta-cliente.component';
+import { ListaClientesComponent } from './pages/lista-cliente/lista-cliente.component';
+import { AltaJugadorComponent } from './pages/alta-jugador/alta-jugador.component';
+import { ListaJugadoresComponent } from './pages/lista-jugadores/lista-jugadores.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { ListadoLibroComponent } from './pages/listado-libro/listado-libro.component';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment.development';
+import { LibroService } from './services/libro.service';
+import { ClienteService } from './services/cliente.service';
+import { JugadorService } from './services/jugador.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaClienteComponent,
+    ListaClientesComponent,
+    AltaJugadorComponent,
+    ListaJugadoresComponent,
+    HomeComponent,
+    MenuComponent,
+    AcercaDeComponent,
+    ListadoLibroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [
+    LibroService,
+    ClienteService,
+    JugadorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
